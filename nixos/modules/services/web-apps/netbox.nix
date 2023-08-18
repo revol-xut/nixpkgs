676a4d15497920bd6705d66bd293e3e93f47a01b
revol-xut <revol-xut@protonmail.com>
2023-08-18 20:40:15 +0200

    netbox: add keycloakClientSecret option

--- nixos/modules/services/web-apps/netbox.nix
+++ nixos/modules/services/web-apps/netbox.nix
@@ -2,7 +2,7 @@
 
 let
   cfg = config.services.netbox;
-  pythonFmt = pkgs.formats.pythonVars {};
+  pythonFmt = pkgs.formats.pythonVars { };
   staticDir = cfg.dataDir + "/static";
 
   settingsFile = pythonFmt.generate "netbox-settings.py" cfg.settings;
@@ -10,6 +10,7 @@ let
     name = "netbox-extraConfig.py";
     text = cfg.extraConfig;
   };
+
   configFile = pkgs.concatText "configuration.py" [ settingsFile extraConfigFile ];
 
   pkg = (cfg.package.overrideAttrs (old: {
@@ -21,13 +22,15 @@ let
   })).override {
     inherit (cfg) plugins;
   };
+
   netboxManageScript = with pkgs; (writeScriptBin "netbox-manage" ''
     #!${stdenv.shell}
     export PYTHONPATH=${pkg.pythonPath}
     sudo -u netbox ${pkg}/bin/netbox "$@"
   '');
 
-in {
+in
+{
   options.services.netbox = {
     enable = lib.mkOption {
       type = lib.types.bool;
@@ -54,7 +57,7 @@ in {
         options = {
           ALLOWED_HOSTS = lib.mkOption {
             type = with lib.types; listOf str;
-            default = ["*"];
+            default = [ "*" ];
             description = lib.mdDoc ''
               A list of valid fully-qualified domain names (FQDNs) and/or IP
               addresses that can be used to reach the NetBox service.
@@ -169,6 +172,14 @@ in {
         AUTH_LDAP_FIND_GROUP_PERMS = True
       '';
     };
+    keycloakClientSecret = mkOption
+      {
+        type = types.path;
+        default = "";
+        description = lib.mdDoc ''
+          File where the secret for the keycloak client secret is located.
+        '';
+      };
   };
 
   config = lib.mkIf cfg.enable {
@@ -194,14 +205,14 @@ in {
         # sections, and it is strongly recommended to use two separate database
         # IDs.
         REDIS = {
-            tasks = {
-                URL = "unix://${config.services.redis.servers.netbox.unixSocket}?db=0";
-                SSL = false;
-            };
-            caching =  {
-                URL = "unix://${config.services.redis.servers.netbox.unixSocket}?db=1";
-                SSL = false;
-            };
+          tasks = {
+            URL = "unix://${config.services.redis.servers.netbox.unixSocket}?db=0";
+            SSL = false;
+          };
+          caching = {
+            URL = "unix://${config.services.redis.servers.netbox.unixSocket}?db=1";
+            SSL = false;
+          };
         };
 
         REMOTE_AUTH_BACKEND = lib.mkIf cfg.enableLdap "netbox.authentication.LDAPBackend";
@@ -227,7 +238,11 @@ in {
       extraConfig = ''
         with open("${cfg.secretKeyFile}", "r") as file:
             SECRET_KEY = file.readline()
-      '';
+      '' + (if cfg.keycloakClientSecret != "" then ''
+        with open("${cfg.keycloakClientSecret}", "r") as file:
+            SOCIAL_AUTH_KEYCLOAK_SECRET = file.readline()
+      '' else "");
+
     };
 
     services.redis.servers.netbox.enable = true;
@@ -253,6 +268,7 @@ in {
       after = [ "network-online.target" "redis-netbox.service" ];
     };
 
+<<<<<<< HEAD
     systemd.services = let
       defaultServiceConfig = {
         WorkingDirectory = "${cfg.dataDir}";
@@ -353,11 +369,99 @@ in {
 
       after = [ "network-online.target" "netbox.service" ];
       wants = [ "network-online.target" ];
+=======
+    systemd.services =
+      let
+        defaultServiceConfig = {
+          WorkingDirectory = "${cfg.dataDir}";
+          User = "netbox";
+          Group = "netbox";
+          StateDirectory = "netbox";
+          StateDirectoryMode = "0750";
+          Restart = "on-failure";
+        };
+      in
+      {
+        netbox-migration = {
+          description = "NetBox migrations";
+          wantedBy = [ "netbox.target" ];
+
+          environment = {
+            PYTHONPATH = pkg.pythonPath;
+          };
+
+          serviceConfig = defaultServiceConfig // {
+            Type = "oneshot";
+            ExecStart = ''
+              ${pkg}/bin/netbox migrate
+            '';
+          };
+        };
+
+        netbox = {
+          description = "NetBox WSGI Service";
+          wantedBy = [ "netbox.target" ];
+          after = [ "netbox-migration.service" ];
+
+          preStart = ''
+            ${pkg}/bin/netbox trace_paths --no-input
+            ${pkg}/bin/netbox collectstatic --no-input
+            ${pkg}/bin/netbox remove_stale_contenttypes --no-input
+          '';
+
+          environment = {
+            PYTHONPATH = pkg.pythonPath;
+          };
+
+          serviceConfig = defaultServiceConfig // {
+            ExecStart = ''
+              ${pkgs.python3Packages.gunicorn}/bin/gunicorn netbox.wsgi \
+                --bind ${cfg.listenAddress}:${toString cfg.port} \
+                --pythonpath ${pkg}/opt/netbox/netbox
+            '';
+          };
+        };
+
+        netbox-rq = {
+          description = "NetBox Request Queue Worker";
+          wantedBy = [ "netbox.target" ];
+          after = [ "netbox.service" ];
+
+          environment = {
+            PYTHONPATH = pkg.pythonPath;
+          };
+
+          serviceConfig = defaultServiceConfig // {
+            ExecStart = ''
+              ${pkg}/bin/netbox rqworker high default low
+            '';
+          };
+        };
+
+        netbox-housekeeping = {
+          description = "NetBox housekeeping job";
+          after = [ "netbox.service" ];
+
+          environment = {
+            PYTHONPATH = pkg.pythonPath;
+          };
+
+          serviceConfig = defaultServiceConfig // {
+            Type = "oneshot";
+            ExecStart = ''
+              ${pkg}/bin/netbox housekeeping
+            '';
+          };
+        };
+      };
+
+    systemd.timers.netbox-housekeeping = {
+      description = "Run NetBox housekeeping job";
+      wantedBy = [ "timers.target" ];
+>>>>>>> dbfbf6a3ac62 (netbox: add keycloakClientSecret option)
 
       timerConfig = {
         OnCalendar = "daily";
-        AccuracySec = "1h";
-        Persistent = true;
       };
     };
 
@@ -366,7 +470,7 @@ in {
       isSystemUser = true;
       group = "netbox";
     };
-    users.groups.netbox = {};
+    users.groups.netbox = { };
     users.groups."${config.services.redis.servers.netbox.user}".members = [ "netbox" ];
   };
 }
